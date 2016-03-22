815fa16ad1ff37aa36ab731c4d25a9947475c226
ch2349 <chenchouhsieh@gmail.com>
2016-03-22 11:30:09 +0800

    add some comment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+# http://code.tutsplus.com/articles/how-to-use-omniauth-to-authenticate-your-users--net-22094
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
