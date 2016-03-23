302abb08dc5332b1aaf2cc54f9d5809528a9ab85
ch2349 <chenchouhsieh@gmail.com>
2016-03-23 11:55:26 +0800

    add production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,9 @@ Rails.application.configure do
 
  
   config.action_mailer.raise_delivery_errors = false
- # config.action_mailer.delivery_method = :smtp
- # host = 'fb-sam-wepay.herokuapp.com'
- # config.action_mailer.default_url_options= {host: host}
+  config.action_mailer.delivery_method = :smtp
+  host = 'fb-sam-wepay.herokuapp.com'
+  config.action_mailer.default_url_options= {host: host}
  # ActionMailer::Base.smtp_settings = {
  #   :address     =>'smtp.sendgrid.net',
  #   :port        =>'587',
